d0cffc05706db34215f24bc8c0a7b14a8d5b10ab
Shuiyee <shuiyee123@163.com>
2026-02-03 03:41:13 +0800

    乐扣第一道题 1.两数之和 Python版本 自写和学习版

---
+++ 1.两数之和/code1.py
@@ -0,0 +1,11 @@
+# 我写的版本
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        tmp = nums[0]
+        for i in range(len(nums)):
+            for g in range(i + 1,len(nums)):
+                if nums[i] + nums[g] == target:
+                    return [i,g]
+
+        
